ef09a0ca2bb7def50d1621eb2f9ee8f7b4015dfb
Billy Laing <blaing@blaing.io>
2024-08-18 21:01:21 -0400

    Add dark mode

--- src/index.js
+++ src/index.js
@@ -4,3 +4,28 @@ import { App } from "./App";
 const container = document.getElementById("app");
 const root = createRoot(container);
 root.render(<App />);
+
+function applyColorSchemePreference() {
+  const darkExpected = window.matchMedia(
+    "(prefers-color-scheme: dark)",
+  ).matches;
+  if (darkExpected) {
+    document.documentElement.classList.add("dark");
+    document.documentElement.style.setProperty("color-scheme", "dark");
+  } else {
+    document.documentElement.classList.remove("dark");
+    document.documentElement.style.setProperty("color-scheme", "light");
+  }
+}
+
+// set listener to update color scheme preference on change
+window
+  .matchMedia("(prefers-color-scheme: dark)")
+  .addEventListener("change", (event) => {
+    const newColorScheme = event.matches ? "dark" : "light";
+    console.log(newColorScheme);
+    applyColorSchemePreference();
+  });
+
+// check color scheme preference on page load
+applyColorSchemePreference();
