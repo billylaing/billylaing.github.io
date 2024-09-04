fee8726e69aa44f56bba15b5af23c79c12182943
Billy Laing <blaing@blaing.io>
2024-09-04 14:39:20 -0400

    Finish migration to Vite

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,36 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { App } from "./App.jsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
+function applyColorSchemePreference() {
+  const darkExpected = window.matchMedia(
+    "(prefers-color-scheme: dark)",
+  ).matches;
+
+  if (darkExpected) {
+    document.documentElement.classList.remove("latte");
+    document.documentElement.classList.add("mocha");
+  } else {
+    document.documentElement.classList.remove("mocha");
+    document.documentElement.classList.add("latte");
+  }
+}
+
+// set listener to update color scheme preference on change
+window
+  .matchMedia("(prefers-color-scheme: dark)")
+  .addEventListener("change", (event) => {
+    const newColorScheme = event.matches ? "dark" : "light";
+    console.log(newColorScheme);
+    applyColorSchemePreference();
+  });
+
+// check color scheme preference on page load
+applyColorSchemePreference();
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-export default {
-  content: ["./src/*.{html,js,ts,tsx,jsx}", "./src/**/*.{html,js,ts,tsx,jsx}"],
-  darkMode: "media",
-  theme: {
-    extend: {},
-  },
-  variants: {},
-  plugins: [],
-};
